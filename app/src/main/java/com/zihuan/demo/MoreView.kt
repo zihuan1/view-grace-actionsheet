9539881accfe0322f608d4d80c351069d03fc2d3
紫幻 <zihuancz@foxmail.com>
2019-12-26 17:13:13 +0800

    test

---
+++ app/src/main/java/com/zihuan/demo/MoreView.kt
@@ -0,0 +1,16 @@
+package com.zihuan.demo
+
+import android.content.Context
+import com.zihuan.view.actionsheet.BaseActionSheet
+import com.zihuan.view.actionsheet.BaseActionView
+
+class MoreView : BaseActionView {
+
+    constructor(context: Context) : super(context) {
+
+    }
+
+    override fun setParentView(parentView: BaseActionSheet<*>) {
+
+    }
+}
